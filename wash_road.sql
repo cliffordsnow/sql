11850a0565f9f2a3016d7811754533ec1a79e51b
Clifford Snow <clifford@snowandsnow.us>
2013-12-11 19:47:34 -0800

    added new script just for king county roads

---
+++ wash_road.sql
@@ -0,0 +1,17 @@
+create or replace function wa_road(pt varchar, fullname, pd varchar,name varchar,typ varchar,sd varchar) RETURNS varchar AS $$
+DECLARE
+  full_name varchar;
+BEGIN
+	IF pt IS NOT NULL THEN
+		full_name := initcap(fullname) || '' || pt;
+	ELSE
+		full_name := expand_direction(pd) || ' ';
+		full_name := full_name ||  initcap(name);
+		full_name := full_name || ' ' || expand_road(typ);
+		IF sd IS NOT NULL THEN
+			full_name := full_name || ' ' || expand_direction(sd);
+		END IF;
+	END IF;
+	RETURN trim(leading ' ' from full_name);
+END;
+$$ LANGUAGE plpgsql;
